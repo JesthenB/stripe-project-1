4497bd29c9deb90c992923c5f390d925a8b70b59
JesthenB <jesthenh@gmail.com>
2023-10-03 20:18:13 -0500

    initial commit, bundled my stripe project with nextjs

---
+++ stripe-project/app/context.js
@@ -0,0 +1,44 @@
+import React, { usestate, usecontext } from "react";
+// import sublinks from "./data";
+
+const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = usestate(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = usestate(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSubmenuOpen,
+        isSidebarOpen,
+        openSubmenu,
+        openSidebar,
+        closeSubmenu,
+        closeSidebar,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+    return usecontext(AppContext)
+}
